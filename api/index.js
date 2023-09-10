4c138f1964fa080403fa01789ac97079148dd9ba
Muhammad Haiqal <muhammadhaiqal1802@gmail.com>
2023-09-11 06:14:53 +0700

    jwt verify function middleware, handle delete request

--- api/index.js
+++ api/index.js
@@ -24,9 +24,41 @@ app.post('/api/login', (req,res) => {
     return u.username === username && u.password === password
   })
   if(user) {
-    res.json(user)
+    // Generate an access token
+    const accessToken = jwt.sign({id:user.id, isAdmin:user.isAdmin}, 'mySecretKey')
+    res.json({
+      username: user.username,
+      isAdmin: user.isAdmin,
+      accessToken,
+    })
   } else {
     res.status(400).json('Username or Password is incorrect')
+  }
+})
+
+const verify = (req,res,next) => {
+  const authHeader = req.headers.authorization
+  if(authHeader) {
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(token, 'mySecretKey', (err,user) => {
+      if(err) {
+        res.status(403).json('Token is not valid')
+      } 
+
+      req.user = user
+      next()
+    })
+  } else {
+    res.status(401).json('You are not authenticated!')
+  }
+}
+
+app.delete('/api/users/:userId', verify, (req,res) => {
+  if(req.user.id === req.params.userId || req.user.isAdmin) {
+    res.status(200).json('User has been deleted')
+  } else {
+    res.status(403).json('You are not allowed to delete this user!')
   }
 })
 
